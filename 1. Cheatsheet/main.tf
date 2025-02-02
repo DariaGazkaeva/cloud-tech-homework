7db938333e42fb834ca885df15589b79ebed3df0
Dasha <d.gazkaeva@gmail.com>
2025-02-02 18:47:57 +0300

    add bucket

--- 1. Cheatsheet/main.tf
+++ 1. Cheatsheet/main.tf
@@ -76,3 +76,22 @@ output "cheatsheet-func-url" {
 resource "telegram_bot_webhook" "webhook" {
   url = "https://api.telegram.org/bot${var.tg_bot_key}/setWebhook?url=https://functions.yandexcloud.net/${yandex_function.cheatsheet-func.id}"
 }
+
+// Grant permissions
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+  folder_id = var.folder_id
+  role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.sa-hw-1.id}"
+}
+
+// Create Static Access Keys
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa-hw-1.id
+  description        = "static access key for object storage"
+}
+
+// Use keys to create bucket
+resource "yandex_storage_bucket" "bucket" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+}
